4b68731ec9d5a1f55f8ed0a490e596da25b87680
ChristmasInAug <barkha1121@naver.com>
2025-03-29 00:48:01 +0900

    Initial commit

---
+++ src/main/java/BoardPractice/BoardPractice01/BoardPractice01Application.java
@@ -0,0 +1,13 @@
+package BoardPractice.BoardPractice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPractice01Application.class, args);
+	}
+
+}
